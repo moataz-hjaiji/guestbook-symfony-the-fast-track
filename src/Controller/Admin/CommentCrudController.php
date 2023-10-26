1fac0c9b6aa064b3243bb1f09ec5fde69a0f9fc7
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2023-10-26 09:38:39 +0100

    feat: upload image on comment , show image on dashboard admin

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
@@ -42,7 +43,9 @@ class CommentCrudController extends AbstractCrudController
                 yield EmailField::new('email');
                 yield TextareaField::new('text')
                         ->hideOnIndex();
-                yield TextField::new('photoFilename')
+                yield ImageField::new('photoFilename')
+                      ->setBasePath('/uploads/photos')
+                      ->setLabel('Photo')
                 ->onlyOnIndex()
                 ;
 
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +18,10 @@ use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
@@ -30,16 +38,30 @@ class ConferenceController extends AbstractController
         ]);
     }
     #[Route('/conference/{slug}',name:"conference")]
-    public function show(Conference $conference,CommentRepository $commentRepository,Request $request):Response
+    public function show(Conference $conference,CommentRepository $commentRepository,Request $request,#[Autowire("%photo_dir%")] string $photoDir):Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class,$comment);
         $offset = max(0,$request->query->getInt('offset'));
         $paginator = $commentRepository->getCommentPaginator($conference,$offset);
-
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setConference($conference);
+            if($photo = $form['photo']->getData()){
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                $photo->move($photoDir,$filename);
+                $comment->setPhotoFilename($filename);
+            }
+            $this->em->persist($comment);
+            $this->em->flush();
+            //return $this->redirectToRoute('conference',["slug"=>$conference->getSlug()]);
+        }
         return $this->render('conference/show.html.twig',[
             "conference"=>$conference,
             "comments"=>$paginator,
             "previous"=>$offset - CommentRepository::PAGINATOR_PER_PAGE,
-            "next"=>min(count($paginator),$offset+CommentRepository::PAGINATOR_PER_PAGE)
+            "next"=>min(count($paginator),$offset+CommentRepository::PAGINATOR_PER_PAGE),
+            "form"=>$form->createView()
         ]);
     }
 }
